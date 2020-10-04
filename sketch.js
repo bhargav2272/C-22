667e66105d724d65529790cccc12ffa2a48a40a4
bhargav2272 <71454431+bhargav2272@users.noreply.github.com>
2020-10-04 20:00:41 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,70 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 400);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 200, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.1
+
+	
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+
+  if(keyDown("Down_Arrow")) {
+	packageSprite.velocityY = 12;
+  }
+
+if(packageSprite.isTouching(groundSprite)){
+	packageSprite.velocityY = 0;
+}
+
+  drawSprites();
+ 
+}
+
+
+
+
+
